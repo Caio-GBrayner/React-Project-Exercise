1a3ab3e62dd907f43406367961c8eaac6841ade1
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-09 10:35:15 -0300

    List and Key to List Items

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function Header(props) {
   return(
-    <haeder>
+    <header>
       <h1>{props.name}</h1>
-    </haeder>
+    </header>
   )
 }
 
@@ -12,6 +12,9 @@ function Main(props) {
   return(
     <main>
       <p>Best recip's for quick and {props.adjective} tasty food.</p>
+      <ul>
+        {props.recipes.map((recipe) => (<li key={recipe.id}> {recipe.title} </li>))}
+      </ul>
     </main>
   )
 }
@@ -23,11 +26,27 @@ function Footer(props) {
     </footer>
   )
 }
+
+const recipes =[
+  "Breakfast Recipe's",
+  "Braunch Recipes's",
+  "Launch Recipe's",
+  "Dinner Recipe's",
+]
+
+const recipeObjects = recipes.map((recipe, recipe_id) => (
+  {
+    id: recipe_id,
+    title: recipe
+  }
+));
+
+
 function App() {
   return (
     <div className="App">
       <Header name="My Recipes"></Header>
-       <Main adjective="delicious"></Main>
+       <Main adjective="delicious" recipes ={recipeObjects}></Main>
        <Footer year={new Date().getFullYear()}></Footer>
     </div>
   );
