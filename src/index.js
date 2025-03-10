535382c4ef75d4edbabf4b488b2b7dee7de36e6e
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-09 22:13:33 -0300

    Arrays and objects

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const [importnatItem, second, third] = [
+  "Book",
+  "Calculator",
+  "Pen",
+  "Ruler",
+]
+
+console.log(importnatItem, second, third);
 
 
 const root = ReactDOM.createRoot(
