a2642056bb5ab8c64b7973f9f314a69508e340ca
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-11 10:54:40 -0300

    useReducer with React

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useReducer} from 'react';
 
 function App() {
 
-  const [first, changeFirst] = useState("In a good mood!");
+const [checked, toggle] = useReducer((checked) => !checked, false);
 
-const [secondary, changeSecondary] = useState("Nice")
-
-  useEffect(() => {
-    console.log(`We are ${first} today!`);
-  },[first]);
-
-  useEffect(() => {
-    console.log(`Its ${secondary} here!`);
-    
-  }, [secondary]);
-  
   return(
   <>
-    <h1>To day i am{first}</h1>
-    <button onClick={() => {changeFirst("In a Happy mood")}}>Change Mood</button>
-    <button onClick={() => {changeFirst("In a Sad mood")}}>Change Mood to Sad</button>
-    <button onClick={() => {changeFirst("In a Frustrated mood")}}>Change Mood to Frustrated</button>
-    
-    <hr></hr>
-
-    <button onClick={() => {changeSecondary("Awsome")}}> Change to Awsome </button>
-    <button onClick={() => {changeSecondary("Great")}}> Change to Great </button>
+   <input type='checkbox' value={checked} onChange={toggle}/>
+   <p>{checked ? "Checked" : "Not Checked"}</p>
   </>
   )
 }
