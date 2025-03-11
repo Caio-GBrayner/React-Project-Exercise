4984a6afdd0936e1ffd28537d5ab43a8a37591aa
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-11 10:41:22 -0300

    Working with useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
 
   const [first, changeFirst] = useState("In a good mood!");
-  console.log(first);
+
+const [secondary, changeSecondary] = useState("Nice")
+
+  useEffect(() => {
+    console.log(`We are ${first} today!`);
+  },[first]);
+
+  useEffect(() => {
+    console.log(`Its ${secondary} here!`);
+    
+  }, [secondary]);
   
   return(
   <>
@@ -12,6 +22,11 @@ function App() {
     <button onClick={() => {changeFirst("In a Happy mood")}}>Change Mood</button>
     <button onClick={() => {changeFirst("In a Sad mood")}}>Change Mood to Sad</button>
     <button onClick={() => {changeFirst("In a Frustrated mood")}}>Change Mood to Frustrated</button>
+    
+    <hr></hr>
+
+    <button onClick={() => {changeSecondary("Awsome")}}> Change to Awsome </button>
+    <button onClick={() => {changeSecondary("Great")}}> Change to Great </button>
   </>
   )
 }
