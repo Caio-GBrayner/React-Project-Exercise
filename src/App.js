5ec59bdadc4a6979c0e67af331a165aa38f98a9a
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-03-12 11:52:51 -0300

    #

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import React, {useState, useEffect} from 'react';
 
 // https://api.github.com/users/Caio-GBrayner
 
-function App(login) {
-  const [data, updateData] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`).then((response) => response.json()).then(updateData);
-  }, [login])
-
-  if(data) {
-    return <div>{JSON.stringify(data)}</div>;
-  }
-  else {
-    <div>No user Available</div>
-  }
+function App() {
+  return (
+    <h1>Welcome to React testing library</h1>
+  );
 }
 
 export default App;
